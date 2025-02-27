8aaad0751bc5d642e17c9edbb8e13316264d5888
Riki-Zf <rikipatande81979@gmail.com>
2025-02-28 07:41:12 +0800

    fix deploy

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Server is running on Vercel!");
+});
+
 //routes
 app.use("/api/recipes", recipeRoutes);
 
