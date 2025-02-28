0c1219d6f6cbc71eead8c3434a39cfdee9968955
Riki-Zf <rikipatande81979@gmail.com>
2025-02-28 18:56:13 +0800

    initial commit

--- AI Recipe/api.js
+++ AI Recipe/api.js
@@ -1,6 +1,6 @@
 export const fetchRecipe = async (ingredients, difficulty, mealType) => {
   try {
-    const response = await fetch("http://localhost:3000/api/recipes", {
+    const response = await fetch("http://localhost:3000/api/recipes" || "https://ai-recipe-oojp.vercel.app/api/recipes", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ ingredients, difficulty, mealType }),
